8a98c87d20e911cfbd99a11499ba7f68eb78c05b
dmitry.lokosov <dmitry.lokosov@profitclicks.ru>
2020-11-19 10:47:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp3.Services;
+using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var mc = new MyClass();
+
+            mc.MyMethod();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp3/Services/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Services
+{
+    internal class MyClass : IMyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public bool MyMethod()
+        {
+            Console.WriteLine("MyClass");
+
+            return true;
+        }
+    }
+}
